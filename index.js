37f571ec39d61b55d4b77fa9cd18e6cd3ef30bbe
Leon de Almeida <leon.dealmeida@shopify.com>
2021-06-07 21:35:42 -0400

    ✨ Add bucket functionality
    Co-authored-by: Louis Marques <louismesmo@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // Classes
 class Sheet {
-  constructor(canvasSize, pixelsPerRow, ctx, currentColor) {
+  constructor(canvasSize, pixelsPerRow, ctx, currentColor, currentTool) {
     this.pixels = [];
     this.ctx = ctx;
     this.currentColor = currentColor;
+    this.currentTool = currentTool;
+    this.pixelsPerRow = pixelsPerRow;
 
     var pixelSize = canvasSize / pixelsPerRow;
     var x,
-      y = 0;
+      y = 0,
+      i = 0;
     for (y = 0; y < pixelsPerRow; y++) {
       for (x = 0; x < pixelsPerRow; x++) {
-        this.pixels.push(new Pixel(pixelSize * x, pixelSize * y, pixelSize));
+        this.pixels.push(new Pixel(pixelSize * x, pixelSize * y, pixelSize, i));
+        i++;
       }
     }
 
@@ -18,7 +22,9 @@ class Sheet {
     this.handleMove = this.handleMove.bind(this);
     this.handleUp = this.handleUp.bind(this);
     this.setColor = this.setColor.bind(this);
-    this.paint = this.paint.bind(this);
+    this.setTool = this.setTool.bind(this);
+    this.execute = this.execute.bind(this);
+    this.fill = this.fill.bind(this);
   }
 
   handleDown(event) {
@@ -29,7 +35,7 @@ class Sheet {
     document.body.classList.add("unselectable");
 
     if (isDown) {
-      this.paint(event);
+      this.execute(event);
     }
   }
   handleUp(event) {
@@ -37,20 +43,27 @@ class Sheet {
     document.body.classList.remove("unselectable");
 
     isDown = false;
-    this.paint(event);
+
+    this.execute(event);
   }
 
-  paint(event) {
+  execute(event) {
     var x = event.pageX - canvasLeft,
       y = event.pageY - canvasTop;
     this.pixels.forEach(function (pixel) {
       if (pixel.collides(x, y)) {
-        if (tool[0].checked) {
-          ctx.globalCompositeOperation = "source-over";
-          pixel.setColor(this.currentColor);
-        } else {
-          ctx.globalCompositeOperation = "destination-out";
-          pixel.setColor("rgba(255,255,255,1)");
+        switch (this.currentTool) {
+          case "pencil":
+            ctx.globalCompositeOperation = "source-over";
+            pixel.setColor(this.currentColor);
+            break;
+          case "eraser":
+            ctx.globalCompositeOperation = "destination-out";
+            pixel.setColor("rgba(255,255,255,1)");
+            break;
+          case "bucket":
+            if (!isDown) this.fill(pixel);
+            break;
         }
         pixel.draw(ctx);
       }
@@ -60,7 +73,41 @@ class Sheet {
   setColor(color) {
     this.currentColor = color;
   }
+  setTool(tool) {
+    this.currentTool = tool;
+  }
+  fill(pixel) {
+    var connectedPixels = [];
+    const firstColor = pixel.color;
+    connectedPixels.push(pixel);
+    
+    while(connectedPixels.length > 0){
+      var currentPixel = connectedPixels.pop();
+      currentPixel.setColor(this.currentColor);
+      currentPixel.draw(ctx);
+      
+      var verifiedPixelIndexes = []
+      if (currentPixel.posX > 0){
+        verifiedPixelIndexes.push(currentPixel.index - 1);
+      }
+      if (currentPixel.posY > 0){
+        verifiedPixelIndexes.push(currentPixel.index - this.pixelsPerRow);
+      }
+      if (currentPixel.posX<((this.pixelsPerRow*currentPixel.size)-currentPixel.size)){
+        verifiedPixelIndexes.push(currentPixel.index + 1);
+      }
+      if (currentPixel.posY<((this.pixelsPerRow*currentPixel.size)-currentPixel.size)){
+        verifiedPixelIndexes.push(currentPixel.index + this.pixelsPerRow);
+      }
 
+      verifiedPixelIndexes.forEach(function (index) {
+        var verifiedPixel = this.pixels[index];
+        if(firstColor == verifiedPixel.color) {
+          connectedPixels.push(verifiedPixel);
+        }
+      }, this);
+    }
+  }
   draw(ctx) {
     this.pixels.forEach(function (pixel) {
       pixel.draw(ctx);
@@ -69,10 +116,11 @@ class Sheet {
 }
 
 class Pixel {
-  constructor(posX, posY, size, color = "rgba(255,255,255,0)") {
+  constructor(posX, posY, size, index, color = "rgba(255,255,255,0)") {
     this.color = color;
     this.posX = posX;
     this.posY = posY;
+    this.index = index;
     this.size = size;
   }
 
@@ -100,11 +148,11 @@ var canvas = document.getElementById("game-canvas"),
   canvasLeft = canvas.offsetLeft + canvas.clientLeft,
   canvasTop = canvas.offsetTop + canvas.clientTop,
   elements = [],
-  tool = document.getElementsByName("tool"),
+  tools = document.getElementsByName("tool"),
   isDown = false;
 
 var ctx = canvas.getContext("2d");
-var sheet = new Sheet(512, 16, ctx, picker.value);
+var sheet = new Sheet(512, 16, ctx, picker.value, "pencil");
 sheet.draw(ctx);
 
 canvas.addEventListener("click", sheet.handleMove, false);
@@ -116,4 +164,9 @@ window.addEventListener("mouseup", (event) => {
 });
 picker.addEventListener("change", (event) => {
   sheet.setColor(event.target.value);
+});
+tools.forEach(function (tool) {
+  tool.addEventListener("click", (event) => {
+    sheet.setTool(tool.value);
+  });
 });
